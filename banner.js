cd45ba21a3d589c3a33fb68248954edd682eb9ab
Allyson <jandreyallyson@gmail.com>
2022-06-01 20:16:11 -0300

    bolinhas adicionadas e icones de botao

--- banner.js
+++ banner.js
@@ -1,30 +1,58 @@
-const slides = document.querySelectorAll(".slide");
-const carrossel = document.getElementById("carrossel");
-const esquerda = document.getElementById("esquerda");
-const direita = document.getElementById("direita");
+var pontos = document.querySelectorAll('.pontos')
+var quantidade = document.querySelectorAll('.carrossel .imagens')
+var atual = 0
+var imagem = document.getElementById('atual')
+var avancar = document.getElementById('avancar')
+var voltar = document.getElementById('voltar')
+var posicao = document.querySelectorAll('.pontos div')
+var passar = true
 
-const contador_slides = slides.length;
-
-let slide_atual = 0;
-
-esquerda.addEventListener("click", () => {
-    slide_atual--;
-    if(slide_atual < 0) {
-        slide_atual = contador_slides - 1;
-    }
-    atualizar_carrossel();
-})
-
-direita.addEventListener("click", () => {
-    slide_atual++;
-    if(slide_atual > contador_slides - 1) {
-        slide_atual = 0
-    }
-    atualizar_carrossel();
-})
-
-function atualizar_carrossel() {
-    carrossel.style.transform = `translateX(${
-        -slide_atual * slides[0].offsetWidth
-        }px)`;
+for (let i = 0; i < quantidade.length; i++) {
+    var div = document.createElemenet('div')
+    div.id = i
+    pontos.appendChild(div)
 }
+
+document.getElementById('0').classList.add('.imagemAtual')
+
+for (let i = 0; i < quantidade.length; i++) {
+    posicao[i].addEventListener ('click', () => {
+        atual = posicao[i].id
+        passar = false
+        carrossel()
+    })
+}
+
+avancar.addEventListener ('click', () => {
+    atual++
+    passar = false
+    carrossel()
+})
+
+voltar.addEventListener ('click', () => {
+    atual--
+    passar = false
+    carrossel()
+})
+
+function carrossel() {
+    if(atual >= quantidade.length) {
+        atual = 0
+    } else if (atual < 0) {
+        atual = quantidade.length-1
+    }
+    document.querySelector('.imagemAtual').classList.remove('imagemAtual')
+    imagem.style.marginLeft = -1024*atual+'px'
+    document.getElementById(atual).classList.add('imagemAtual')
+}
+
+setInterval(() => {
+    if(passar){
+    atual++
+    carrossel()
+    } else {
+        passar = true
+    }
+}, 4000)
+
+carrossel()

1e559f83a859b9502c275ed4b83874b54f7a11ac
Allyson <jandreyallyson@gmail.com>
2022-06-02 22:39:54 -0300

    banner refeito e funcional

--- banner.js
+++ banner.js
@@ -1,58 +1,15 @@
-var pontos = document.querySelectorAll('.pontos')
-var quantidade = document.querySelectorAll('.carrossel .imagens')
-var atual = 0
-var imagem = document.getElementById('atual')
-var avancar = document.getElementById('avancar')
-var voltar = document.getElementById('voltar')
-var posicao = document.querySelectorAll('.pontos div')
-var passar = true
+let count = 1
+document.getElementById("radio1").checked = true
 
-for (let i = 0; i < quantidade.length; i++) {
-    var div = document.createElemenet('div')
-    div.id = i
-    pontos.appendChild(div)
-}
+setInterval(function(){
+    nextImage();
+},4000)
 
-document.getElementById('0').classList.add('.imagemAtual')
-
-for (let i = 0; i < quantidade.length; i++) {
-    posicao[i].addEventListener ('click', () => {
-        atual = posicao[i].id
-        passar = false
-        carrossel()
-    })
-}
-
-avancar.addEventListener ('click', () => {
-    atual++
-    passar = false
-    carrossel()
-})
-
-voltar.addEventListener ('click', () => {
-    atual--
-    passar = false
-    carrossel()
-})
-
-function carrossel() {
-    if(atual >= quantidade.length) {
-        atual = 0
-    } else if (atual < 0) {
-        atual = quantidade.length-1
+function nextImage() {
+    count++;
+    if(count > 4) {
+        count = 1;
     }
-    document.querySelector('.imagemAtual').classList.remove('imagemAtual')
-    imagem.style.marginLeft = -1024*atual+'px'
-    document.getElementById(atual).classList.add('imagemAtual')
+
+    document.getElementById("radio"+count).checked = true
 }
-
-setInterval(() => {
-    if(passar){
-    atual++
-    carrossel()
-    } else {
-        passar = true
-    }
-}, 4000)
-
-carrossel()
